f617556665349a25cadb32e9aff8602ed1fb6eb6
simplifywoopii <simplifywoopii@gmail.com>
2024-05-17 10:39:26 +0900

    karpenter install

---
+++ karpenter/karpenter.sh
@@ -0,0 +1,14 @@
+export KARPENTER_NAMESPACE="kube-system"
+export KARPENTER_VERSION="0.36.1"
+export K8S_VERSION="1.29"
+
+export AWS_PARTITION="aws" # if you are not using standard partitions, you may need to configure to aws-cn / aws-us-gov
+export CLUSTER_NAME="apple-testbed-eksCluster"
+export AWS_DEFAULT_REGION="ap-northeast-2"
+export AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
+export TEMPOUT="$(mktemp)"
+export ARM_AMI_ID="$(aws ssm get-parameter --name /aws/service/eks/optimized-ami/${K8S_VERSION}/amazon-linux-2-arm64/recommended/image_id --query Parameter.Value --output text)"
+export AMD_AMI_ID="$(aws ssm get-parameter --name /aws/service/eks/optimized-ami/${K8S_VERSION}/amazon-linux-2/recommended/image_id --query Parameter.Value --output text)"
+export GPU_AMI_ID="$(aws ssm get-parameter --name /aws/service/eks/optimized-ami/${K8S_VERSION}/amazon-linux-2-gpu/recommended/image_id --query Parameter.Value --output text)"
+
+
